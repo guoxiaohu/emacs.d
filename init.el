1bea078e45b4fd0ff0fd922cccc9265346b030b4
Xiaohu Guo <xiaohu.guo@stfc.ac.uk>
2020-10-02 21:37:50 +0100

    format updates

--- init.el
+++ init.el
@@ -17,14 +17,12 @@
 
 (require 'setup-general)
 (if (version< emacs-version "24.4")
-    (require 'setup-ivy-counsel)
+  (require 'setup-ivy-counsel)
   (require 'setup-helm)
   (require 'setup-helm-gtags))
 ;; (require 'setup-ggtags)
 (require 'setup-cedet)
 (require 'setup-editing)
-
-
 
 ;; function-args
 ;; (require 'function-args)
@@ -38,10 +36,19 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (lsp-latex lsp-jedi lsp-mode workgroups2 zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: workgroups2               ;;
+;;                                    ;;
+;; GROUP: Convenience -> Workgroups   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'workgroups2)
+;; Change some settings
+(workgroups-mode 1)
